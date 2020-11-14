860bd25f57bb236a60d85b6f86fb33fb48656b69
hikipuro <hikipuro@hotmail.com>
2020-11-14 16:24:08 +0900

    fix FormattableString

--- BasicClasses/Polyfills/System/FormattableString.cs
+++ BasicClasses/Polyfills/System/FormattableString.cs
@@ -8,13 +8,10 @@ namespace System {
 
 		public static string Invariant(FormattableString formattable) {
 			return string.Format(
-				CultureInfo.InstalledUICulture,
+				CultureInfo.InvariantCulture,
 				formattable.Format,
 				formattable.GetArguments()
 			);
-		}
-
-		protected FormattableString() {
 		}
 
 		public static string CurrentCulture(FormattableString formattable) {
@@ -25,14 +22,16 @@ namespace System {
 			);
 		}
 
+		protected FormattableString() {
+		}
+
 		public abstract object GetArgument(int index);
 		public abstract object[] GetArguments();
+		public abstract string ToString(IFormatProvider formatProvider);
 
 		public override string ToString() {
 			return ToString(CultureInfo.CurrentCulture);
 		}
-
-		public abstract string ToString(IFormatProvider formatProvider);
 
 		public string ToString(string format, IFormatProvider formatProvider) {
 			return ToString(formatProvider);
